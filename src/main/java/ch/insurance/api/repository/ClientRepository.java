4c5474aded66455d59e088d956a9c7e990702937
Aymeriko <mric.bouteiller@gmail.com>
2025-10-18 22:39:26 +0200

    feat() : add controller and service layers

---
+++ src/main/java/ch/insurance/api/repository/ClientRepository.java
@@ -0,0 +1,15 @@
+package ch.insurance.api.repository;
+
+import ch.insurance.api.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
